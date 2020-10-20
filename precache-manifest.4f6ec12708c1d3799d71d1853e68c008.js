2a23777e4228ddf210bf20c1224261965eeb2182
biio-front <bohwajung@naver.com>
2020-10-20 17:55:22 +0900

    Updates

---
+++ precache-manifest.4f6ec12708c1d3799d71d1853e68c008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d42a7b68503dd1b72c637256698604",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "f04ce316aacf1e86cccc",
+    "url": "/movie-app/static/css/main.081a1cf8.chunk.css"
+  },
+  {
+    "revision": "b9cb88537a0f4167021b",
+    "url": "/movie-app/static/js/2.1c14cd22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.1c14cd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04ce316aacf1e86cccc",
+    "url": "/movie-app/static/js/main.43448913.chunk.js"
+  },
+  {
+    "revision": "daeaf96d14270f7d7af2",
+    "url": "/movie-app/static/js/runtime-main.2be5c0aa.js"
+  }
+]);
